c885e076dbf29cead490dc867e9030561f831790
Abhijit Sharang <abhisg@stanford.edu>
2016-10-23 21:25:22 -0700

    -

--- examples/imdb_cnn.py
+++ examples/imdb_cnn.py
@@ -18,18 +18,20 @@ from keras.layers import Convolution1D, MaxPooling1D
 from keras.datasets import imdb
 from keras import backend as K
 
+import tensorflow as tf
+tf.python.control_flow_ops = tf
 
 # set parameters:
 max_features = 5000
 maxlen = 400
 batch_size = 32
 embedding_dims = 50
-nb_filter = 250
+nb_filter = 1000
 filter_length = 3
-hidden_dims = 250
-nb_epoch = 2
+hidden_dims = 500
+nb_epoch = 10
 
-print('Loading data...')
+"""print('Loading data...')
 (X_train, y_train), (X_test, y_test) = imdb.load_data(nb_words=max_features)
 print(len(X_train), 'train sequences')
 print(len(X_test), 'test sequences')
@@ -38,27 +40,41 @@ print('Pad sequences (samples x time)')
 X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
 X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
 print('X_train shape:', X_train.shape)
-print('X_test shape:', X_test.shape)
+print('X_test shape:', X_test.shape)"""
 
+X_train = np.load('/lfs/local/0/abhisg/vw/driver_data/driver_data_X_train')
+X_train = X_train.reshape(X_train.shape[0],1,X_train.shape[1])
+y_train = np.load('/lfs/local/0/abhisg/vw/driver_data/driver_data_Y_train')
+X_test = np.load('/lfs/local/0/abhisg/vw/driver_data/driver_data_X_test')
+X_test = X_test.reshape(X_test.shape[0],1,X_test.shape[1])
+y_test = np.load('/lfs/local/0/abhisg/vw/driver_data/driver_data_Y_test')
+
+#X_train = X_train.reshape(-1, 1, X_train.shape[1])
+#X_test = X_test.reshape(-1, 1, X_train.shape[1])
 print('Build model...')
 model = Sequential()
-
+print(X_train.shape[1:])
+model.add(Convolution1D(nb_filter,filter_length,input_shape=X_train.shape[1:],border_mode='same'))
+print(model.output_shape)
+#model.add(Dense(embedding_dims,input_dim=X_train.shape[1]))
 # we start off with an efficient embedding layer which maps
 # our vocab indices into embedding_dims dimensions
-model.add(Embedding(max_features,
-                    embedding_dims,
-                    input_length=maxlen,
-                    dropout=0.2))
+#model.add(Embedding(max_features,
+#                    embedding_dims,
+#                    input_length=maxlen,
+#                    dropout=0.2))
 
 # we add a Convolution1D, which will learn nb_filter
 # word group filters of size filter_length:
-model.add(Convolution1D(nb_filter=nb_filter,
-                        filter_length=filter_length,
-                        border_mode='valid',
-                        activation='relu',
-                        subsample_length=1))
+#model.add(Convolution1D(nb_filter=nb_filter,
+#                        filter_length=filter_length,
+#                        border_mode='valid',
+#                        activation='relu',
+#                        subsample_length=1))
 # we use max pooling:
-model.add(MaxPooling1D(pool_length=model.output_shape[1]))
+model.add(MaxPooling1D(pool_length=1))
+model.add(Convolution1D(nb_filter/2,filter_length,border_mode='same'))
+model.add(MaxPooling1D(pool_length=1))
 
 # We flatten the output of the conv layer,
 # so that we can add a vanilla dense layer:
@@ -66,11 +82,11 @@ model.add(Flatten())
 
 # We add a vanilla hidden layer:
 model.add(Dense(hidden_dims))
-model.add(Dropout(0.2))
+model.add(Dropout(0.1))
 model.add(Activation('relu'))
 
 # We project onto a single unit output layer, and squash it with a sigmoid:
-model.add(Dense(1))
+model.add(Dense(2))
 model.add(Activation('sigmoid'))
 
 model.compile(loss='binary_crossentropy',
--- examples/imdb_cnn_lstm.py
+++ examples/imdb_cnn_lstm.py
@@ -14,7 +14,8 @@ from keras.layers import Embedding
 from keras.layers import LSTM
 from keras.layers import Convolution1D, MaxPooling1D
 from keras.datasets import imdb
-
+import tensorflow as tf
+tf.python.control_flow_ops = tf
 
 # Embedding
 max_features = 20000
@@ -22,16 +23,16 @@ maxlen = 100
 embedding_size = 128
 
 # Convolution
-filter_length = 5
-nb_filter = 64
-pool_length = 4
+filter_length = 3
+nb_filter = 800
+pool_length = 1
 
 # LSTM
-lstm_output_size = 70
+lstm_output_size = 200
 
 # Training
-batch_size = 30
-nb_epoch = 2
+batch_size = 20
+nb_epoch = 500
 
 '''
 Note:
@@ -40,29 +41,39 @@ Only 2 epochs are needed as the dataset is very small.
 '''
 
 print('Loading data...')
-(X_train, y_train), (X_test, y_test) = imdb.load_data(nb_words=max_features)
-print(len(X_train), 'train sequences')
-print(len(X_test), 'test sequences')
+#(X_train, y_train), (X_test, y_test) = imdb.load_data(nb_words=max_features)
+#print(len(X_train), 'train sequences')
+#print(len(X_test), 'test sequences')
+#print(X_train.shape, X_test.shape)
+#print('Pad sequences (samples x time)')
+#X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
+#X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
+#print('X_train shape:', X_train.shape)
+#print('X_test shape:', X_test.shape)
 
-print('Pad sequences (samples x time)')
-X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
-X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
-print('X_train shape:', X_train.shape)
-print('X_test shape:', X_test.shape)
+X_train = np.load('/lfs/local/0/abhisg/vw/driver_data/full_driver_data_X_train')
+X_train = X_train.reshape(X_train.shape[0],1,X_train.shape[1])
+y_train = np.load('/lfs/local/0/abhisg/vw/driver_data/full_driver_data_Y_train')
+X_test = np.load('/lfs/local/0/abhisg/vw/driver_data/full_driver_data_X_test')
+X_test = X_test.reshape(X_test.shape[0],1,X_test.shape[1])
+y_test = np.load('/lfs/local/0/abhisg/vw/driver_data/full_driver_data_Y_test')
 
 print('Build model...')
 
 model = Sequential()
-model.add(Embedding(max_features, embedding_size, input_length=maxlen))
-model.add(Dropout(0.25))
-model.add(Convolution1D(nb_filter=nb_filter,
-                        filter_length=filter_length,
-                        border_mode='valid',
-                        activation='relu',
-                        subsample_length=1))
-model.add(MaxPooling1D(pool_length=pool_length))
+#model.add(Embedding(max_features, embedding_size, input_length=maxlen))
+model.add(Convolution1D(nb_filter,filter_length,input_shape=X_train.shape[1:],border_mode='same'))
+#model.add(Convolution1D(nb_filter,filter_length,input_shape=(5,X_train.shape[-1]),border_mode='same'))
+#model.add(Convolution1D(nb_filter=nb_filter,
+#                        filter_length=filter_length,
+#                        border_mode='valid',
+#                        activation='relu',
+#                        input_shape=X_train.shape[1:]
+#                        ))
+#model.add(MaxPooling1D(pool_length=pool_length))
 model.add(LSTM(lstm_output_size))
-model.add(Dense(1))
+model.add(Dropout(0.25))
+model.add(Dense(2))
 model.add(Activation('sigmoid'))
 
 model.compile(loss='binary_crossentropy',
@@ -73,5 +84,17 @@ print('Train...')
 model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=nb_epoch,
           validation_data=(X_test, y_test))
 score, acc = model.evaluate(X_test, y_test, batch_size=batch_size)
+val = model.predict(X_test)
+counts = [0,0,0,0]
+for i in xrange(len(val)):
+    if y_test[i][1] == 1 and val[i][0] < val[i][1]:
+        counts[0] += 1
+    elif y_test[i][1] == 1 and val[i][0] >= val[i][1]:
+        counts[1] += 1
+    elif y_test[i][0] == 1 and val[i][0] > val[i][1]:
+        counts[2] += 1
+    elif y_test[i][0] == 1 and val[i][0] <= val[i][1]:
+        counts[3] += 1
+print(counts)
 print('Test score:', score)
 print('Test accuracy:', acc)
